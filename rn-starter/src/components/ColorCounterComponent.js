7524f8cf929bd8fcf7abd913b0f4ec4272bee5d1
Dominic Talbot <hello@domtalbot.co.uk>
2022-01-21 14:20:22 +0000

    further module work

--- rn-starter/src/components/ColorCounterComponent.js
+++ rn-starter/src/components/ColorCounterComponent.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import {View, Text, Image, StyleSheet, Button} from 'react-native';
 
+const validateChange = (value) => {
+    return !(value < 0 || value > 255);
+}
+
+const changeColor = (props, value) => {
+    if (validateChange(value)) {
+        props.onChange(value);
+    }
+}
+
 const ColorCounterComponent = (props) => {
     return (
         <View style={styles.view}>
             <Text style={styles.text}>{props.title} Current Value: {props.value}</Text>
             <Button
                 title={`More ${props.title} +`}
-                onPress={() => {
-                    props.onChange(props.value + 1)
-                }}
+                onPress={() => changeColor(props,props.value + 1)}
             />
             <Button
                 title={`Less ${props.title} -`}
-                onPress={() => {
-                    props.onChange(props.value - 1)
-                }}
+                onPress={() => changeColor(props, props.value - 1)}
             />
         </View>
     );
